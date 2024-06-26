668b19b032d3b8b2f5759e7e59c47325d345a676
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 18:54:00 +0200

    feat: Added Flight repository.

---
+++ AirlineCompany3/AirlineCompany3/Repository/FlightRepository.cs
@@ -0,0 +1,16 @@
+﻿using AirlineCompany3.Model.Domain;
+using AirlineCompany3.Repository.DatabaseContext;
+using AirlineCompany3.Repository.Interface;
+
+namespace AirlineCompany3.Repository
+{
+    public class FlightRepository : Repository<Flight>, IFlightRepository
+    {
+        private ServerDatabaseContext _db;
+
+        public FlightRepository(ServerDatabaseContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
