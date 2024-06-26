70e52dc325c3193070a209e4dcfe405009f63f09
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 19:21:55 +0200

    feat: Added Flight creation GraphQL type.

---
+++ AirlineCompany3/AirlineCompany3/Resolver/Type/FlightCreationType.cs
@@ -0,0 +1,24 @@
+﻿using AirlineCompany3.Model.Dto;
+
+namespace AirlineCompany3.Resolver.Type
+{
+    public class FlightCreationType : InputObjectType<FlightCreationDto>
+    {
+        protected override void Configure(IInputObjectTypeDescriptor<FlightCreationDto> descriptor)
+        {
+            descriptor.Name("Flight");
+            descriptor.Field(f => f.ScheduledDeparture).Type<NonNullType<DateTimeType>>();
+            descriptor.Field(f => f.ScheduledArrival).Type<NonNullType<DateTimeType>>();
+            descriptor.Field(f => f.BaggageGuidelines).Type<NonNullType<StringType>>();
+            descriptor.Field(f => f.KidsDiscount).Type<NonNullType<FloatType>>();
+            descriptor.Field(f => f.StartingPointIata).Type<NonNullType<StringType>>();
+            descriptor.Field(f => f.EndingPointIata).Type<NonNullType<StringType>>();
+            descriptor.Field(f => f.EconomyCount).Type<NonNullType<IntType>>();
+            descriptor.Field(f => f.EconomyPrice).Type<NonNullType<IntType>>();
+            descriptor.Field(f => f.BusinessCount).Type<NonNullType<IntType>>();
+            descriptor.Field(f => f.BusinessPrice).Type<NonNullType<IntType>>();
+            descriptor.Field(f => f.FirstCount).Type<NonNullType<IntType>>();
+            descriptor.Field(f => f.FirstPrice).Type<NonNullType<IntType>>();
+        }
+    }
+}
