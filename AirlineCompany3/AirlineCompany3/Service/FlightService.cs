b8f627a3f25947da181a4f7d958e3bcf3ec882f6
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 19:03:56 +0200

    feat: Added Flight Service.

---
+++ AirlineCompany3/AirlineCompany3/Service/FlightService.cs
@@ -0,0 +1,19 @@
+﻿using AirlineCompany3.Repository;
+using AirlineCompany3.Repository.Interface;
+using AirlineCompany3.Service.Interface;
+using AutoMapper;
+
+namespace AirlineCompany3.Service
+{
+    public class FlightService : IFlightService
+    {
+        private readonly IMapper _mapper;
+        private readonly IFlightRepository _flightRepository;
+
+        public FlightService(IMapper mapper, IFlightRepository flightRepository)
+        {
+            _mapper = mapper;
+            _flightRepository = flightRepository;
+        }
+    }
+}
