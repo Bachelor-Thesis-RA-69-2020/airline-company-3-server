6547064fd123491ea2b171e6f64285e4c3a38e12
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 19:19:36 +0200

    feat: Added Flight creation in Flight service.

--- AirlineCompany3/AirlineCompany3/Service/FlightService.cs
+++ AirlineCompany3/AirlineCompany3/Service/FlightService.cs
@@ -1,6 +1,9 @@
-﻿using AirlineCompany3.Repository;
+﻿using AirlineCompany3.Model.Domain;
+using AirlineCompany3.Model.Dto;
+using AirlineCompany3.Repository;
 using AirlineCompany3.Repository.Interface;
 using AirlineCompany3.Service.Interface;
+using AirlineCompany3.Utility;
 using AutoMapper;
 
 namespace AirlineCompany3.Service
@@ -9,11 +12,49 @@ namespace AirlineCompany3.Service
     {
         private readonly IMapper _mapper;
         private readonly IFlightRepository _flightRepository;
+        private readonly IAirportRepository _airportRepository;
 
-        public FlightService(IMapper mapper, IFlightRepository flightRepository)
+        public FlightService(IMapper mapper, IFlightRepository flightRepository, IAirportRepository airportRepository)
         {
             _mapper = mapper;
             _flightRepository = flightRepository;
+            _airportRepository = airportRepository;
         }
-    }
+
+        public String create(FlightCreationDto flightDto)
+        {
+            try
+            {
+                Flight flight = _mapper.Map<Flight>(flightDto);
+
+                Airport startingPoint = _airportRepository.Get(filter: a => a.Iata == flightDto.StartingPointIata, orElseThrow: true);
+                Airport endingPoint = _airportRepository.Get(filter: a => a.Iata == flightDto.EndingPointIata, orElseThrow: true);
+                flight.SetRelation(startingPoint, endingPoint);
+
+                flight.SerialNumber = GenerateSerialNumber(flight);
+
+                flight.Validate();
+                _flightRepository.Create(flight);
+
+                return "Flight created.";
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private String GenerateSerialNumber(Flight flight)
+        {
+            String id = flight.ScheduledDeparture.ToString() +
+                flight.ScheduledArrival.ToString() +
+                flight.StartingPoint.Iata +
+                flight.EndingPoint.Iata;
+
+            String hash = Hasher.Hash(id);
+
+            String serialNumber = "AL3-" + hash.Substring(0, 10).ToUpper();
+
+            return serialNumber;
+        }
 }
