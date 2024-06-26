900806316876592e3130a1ab19d1963d0333dd7b
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 18:44:27 +0200

    feat: Created ASP .NET Web Application project.

---
+++ AirlineCompany3/AirlineCompany3/Service/Interface/IAirportService.cs
@@ -0,0 +1,10 @@
+﻿using AirlineCompany3.Model.Dto;
+
+namespace AirlineCompany3.Service.Interface
+{
+    public interface IAirportService
+    {
+        List<AirportDto> getAll();
+        List<AirportDto> search(String filter);
+    }
+}
