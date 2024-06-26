b1d629e75d337ca0f5b64c8387cbfd70231e81eb
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 18:47:33 +0200

    feat: Added Airport repository.

---
+++ AirlineCompany3/AirlineCompany3/Repository/AirportRepository.cs
@@ -0,0 +1,16 @@
+﻿using AirlineCompany3.Model.Domain;
+using AirlineCompany3.Repository.DatabaseContext;
+using AirlineCompany3.Repository.Interface;
+
+namespace AirlineCompany3.Repository
+{
+    public class AirportRepository : Repository<Airport>, IAirportRepository
+    {
+        private ServerDatabaseContext _db;
+
+        public AirportRepository(ServerDatabaseContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ AirlineCompany3/AirlineCompany3/Repository/Interface/IAirportRepository.cs
@@ -0,0 +1,8 @@
+﻿using AirlineCompany3.Model.Domain;
+
+namespace AirlineCompany3.Repository.Interface
+{
+    public interface IAirportRepository : IRepository<Airport>
+    {
+    }
+}
