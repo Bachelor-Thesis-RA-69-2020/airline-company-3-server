fe637a3cc02c58b783ae999b7a74cfd633e4f51e
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:22:54 +0200

    feat: Added Ticket Pricing.

---
+++ AirlineCompany3/AirlineCompany3/Model/Domain/TicketPricing.cs
@@ -0,0 +1,17 @@
+﻿namespace AirlineCompany3.Model.Domain
+{
+    public class TicketPricing
+    {
+        public int EconomyCount { get; set; }
+        public float EconomyPrice { get; set; }
+        public int BusinessCount { get; set; }
+        public float BusinessPrice { get; set; }
+        public int FirstCount { get; set; }
+        public float FirstPrice { get; set; }
+
+        public TicketPricing()
+        {
+
+        }
+    }
+}
