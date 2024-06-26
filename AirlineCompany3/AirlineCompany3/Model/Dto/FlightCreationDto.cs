eaa33248ffbe283fdcd7f11f648c5b828a9929d2
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 19:00:29 +0200

    feat: Added Flight creation DTO.

---
+++ AirlineCompany3/AirlineCompany3/Model/Dto/FlightCreationDto.cs
@@ -0,0 +1,18 @@
+﻿namespace AirlineCompany3.Model.Dto
+{
+    public class FlightCreationDto
+    {
+        public DateTime ScheduledDeparture { get; set; }
+        public DateTime ScheduledArrival { get; set; }
+        public string BaggageGuidelines { get; set; }
+        public float KidsDiscount { get; set; }
+        public string StartingPointIata{ get; set; }
+        public string EndingPointIata { get; set; }
+        public int EconomyCount { get; set; }
+        public int EconomyPrice { get; set; }
+        public int BusinessCount { get; set; }
+        public int BusinessPrice { get; set; }
+        public int FirstCount { get; set; }
+        public int FirstPrice { get; set; }
+    }
+}
