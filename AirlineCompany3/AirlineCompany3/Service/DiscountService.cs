689089e48728410d7d42bd89bf003107a374fb99
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 22:05:24 +0200

    feat: Added Discount creation.

--- AirlineCompany3/AirlineCompany3/Service/DiscountService.cs
+++ AirlineCompany3/AirlineCompany3/Service/DiscountService.cs
@@ -1,6 +1,10 @@
-﻿using AirlineCompany3.Repository.Interface;
+﻿using AirlineCompany3.Model.Domain;
+using AirlineCompany3.Model;
+using AirlineCompany3.Repository;
+using AirlineCompany3.Repository.Interface;
 using AirlineCompany3.Service.Interface;
 using AutoMapper;
+using AirlineCompany3.Model.Dto;
 
 namespace AirlineCompany3.Service
 {
@@ -13,6 +17,26 @@ namespace AirlineCompany3.Service
         {
             _mapper = mapper;
             _flightRepository = flightRepository;
+        }
+
+        public MessageDto create(DiscountCreationDto discountDto)
+        {
+            try
+            {
+                Discount discount = _mapper.Map<Discount>(discountDto);
+
+                Flight flight = _flightRepository.Get(filter: f => f.SerialNumber == discountDto.FlightSerialNumber, includedProperties: "Tickets, Discounts", orElseThrow: true);
+
+                flight.AddDiscount(discount);
+
+                _flightRepository.Update(flight);
+
+                return new MessageDto("Discount created.");
+            }
+            catch (Exception ex)
+            {
+                return new MessageDto(ex.Message);
+            }
         }
     }
 }
