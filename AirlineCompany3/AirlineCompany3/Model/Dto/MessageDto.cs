fb2ce58768f56220522725090480bf0e98d8296f
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:03:00 +0200

    feat: Added Message DTO.

---
+++ AirlineCompany3/AirlineCompany3/Model/Dto/MessageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AirlineCompany3.Model.Dto
+{
+    public class MessageDto
+    {
+        public String Message { get; set; }
+
+        public MessageDto(String message)
+        {
+            Message = message;
+        }
+    }
+}
