82dfd7c6a68702d88335db4323995a9ee40473d1
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 19:08:38 +0200

    feat: Added Flight auto mapper.

---
+++ AirlineCompany3/AirlineCompany3/Model/MappingProfile/FlightProfile.cs
@@ -0,0 +1,19 @@
+﻿using AirlineCompany3.Model.Domain;
+using AirlineCompany3.Model.Dto;
+using AutoMapper;
+
+namespace AirlineCompany3.Model.MappingProfile
+{
+    public class FlightProfile : Profile
+    {
+        public FlightProfile()
+        {
+            CreateMaps();
+        }
+
+        private void CreateMaps()
+        {
+            CreateMap<FlightCreationDto, Flight>();
+        }
+    }
+}
