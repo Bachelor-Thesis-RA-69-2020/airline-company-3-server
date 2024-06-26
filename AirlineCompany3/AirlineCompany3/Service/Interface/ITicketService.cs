d3d68e039b37859fe6d64bdcbbad94a649ff16da
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:23:28 +0200

    feat: Added Ticket Service interface.

---
+++ AirlineCompany3/AirlineCompany3/Service/Interface/ITicketService.cs
@@ -0,0 +1,9 @@
+﻿using AirlineCompany3.Model.Domain;
+
+namespace AirlineCompany3.Service.Interface
+{
+    public interface ITicketService
+    {
+        List<Ticket> GenerateTickets(FlightService flight, TicketPricing pricing);
+    }
+}
