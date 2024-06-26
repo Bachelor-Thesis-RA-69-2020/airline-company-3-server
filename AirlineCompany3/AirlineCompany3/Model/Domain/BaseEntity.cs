22cfbb0044823d4f1447c09820f4e823440394a7
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 18:45:18 +0200

    feat: Added BaseEntity class.

---
+++ AirlineCompany3/AirlineCompany3/Model/Domain/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirlineCompany3.Models.Domain
+{
+    public class BaseEntity
+    {
+        [Key]
+        public string Id { get; set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public virtual void Validate()
+        {
+            var context = new ValidationContext(this, serviceProvider: null, items: null);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(this, context, results, validateAllProperties: true);
+
+            if (!isValid)
+            {
+                var errorMessage = new System.Text.StringBuilder();
+                foreach (var validationResult in results)
+                {
+                    errorMessage.Append($"Validation: {validationResult.ErrorMessage}; ");
+                }
+
+                throw new ArgumentException(errorMessage.ToString());
+            }
+        }
+    }
+}
