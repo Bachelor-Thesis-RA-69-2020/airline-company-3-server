f52c094d9050218ad82b567eca730f1b3f8d77b8
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:01:43 +0200

    fix: Fixed data initialization.

--- AirlineCompany3/AirlineCompany3/Program.cs
+++ AirlineCompany3/AirlineCompany3/Program.cs
@@ -6,6 +6,8 @@ using AirlineCompany3.Service;
 using AirlineCompany3.Resolver.Query;
 using AirlineCompany3.Resolver.Type;
 using Microsoft.EntityFrameworkCore;
+using AirlineCompany3.Resolver.Mutation;
+using AirlineCompany3.Repository.DataInitialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,9 +24,22 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddGraphQLServer()
                 .AddQueryType<AirportQuery>()
-                .AddType<AirportType>();
+                .AddType<AirportType>()
+                .AddMutationType<FlightMutation>()
+                .AddType<FlightCreationType>()
+                .AddType<MessageType>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var dbContext = services.GetRequiredService<ServerDatabaseContext>();
+
+    var initializer = new AirportDataInitializer(dbContext);
+    initializer.Initialize();
+}
 
 app.MapGraphQL();
 app.MapGraphQLHttp();
