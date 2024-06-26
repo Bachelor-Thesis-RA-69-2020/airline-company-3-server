a242047e1f4bb355076707eb0b70902606624306
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:08:03 +0200

    feat: Added Ticket class.

---
+++ AirlineCompany3/AirlineCompany3/Model/Domain/Ticket.cs
@@ -0,0 +1,48 @@
+﻿using AirlineCompany3.Models.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineCompany3.Model.Domain
+{
+    public class Ticket : BaseEntity
+    {
+        [Required(ErrorMessage = "Validation: Ticket code is required")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "Validation: Ticket price is required")]
+        [Range(0, float.MaxValue, ErrorMessage = "Validation: Ticket price must be greater than or equal to 0")]
+        public float Price { get; set; }
+
+        [Required(ErrorMessage = "Validation: Ticket type is required")]
+        public FlightClass Type { get; set; }
+
+        public bool IsBought { get; set; }
+
+        //public Booking Booking { get; set; }
+
+        public Ticket() : base() 
+        {
+        }
+
+        public Ticket(string code, float price, FlightClass type) : base()
+        {
+            Code = code;
+            Price = price;
+            Type = type;
+            IsBought = false;
+        }
+        
+
+        //public void Buy(Booking booking)
+        //{
+        //    IsBought = true;
+        //    Booking = booking;
+        //}
+    }
+
+    public enum FlightClass
+    {
+        Economy,
+        Business,
+        First
+    }
+}
