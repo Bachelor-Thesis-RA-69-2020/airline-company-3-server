71c2512b6049710c7823b2f7957e5743b95945ea
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:04:08 +0200

    refactor: Renamed Airports query.

--- AirlineCompany3/AirlineCompany3/Resolver/Query/AirportQuery.cs
+++ AirlineCompany3/AirlineCompany3/Resolver/Query/AirportQuery.cs
@@ -6,7 +6,7 @@ namespace AirlineCompany3.Resolver.Query
     {
         protected override void Configure(IObjectTypeDescriptor descriptor)
         {
-            descriptor.Field("airports")
+            descriptor.Field("GetAirports")
                   .Argument("filter", a => a.Type<StringType>().DefaultValue(null))
                   .ResolveWith<AirportResolver>(r => r.GetAirports(default, default))
                   .Type<ListType<NonNullType<AirportType>>>();
