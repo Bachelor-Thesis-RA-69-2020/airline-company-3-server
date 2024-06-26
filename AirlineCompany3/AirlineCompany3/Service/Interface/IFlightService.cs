465351f2d5ecc43e57b8ed2dc0d6576ebe6a4f6f
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 19:02:09 +0200

    feat: Added Flight service interface.

---
+++ AirlineCompany3/AirlineCompany3/Service/Interface/IFlightService.cs
@@ -0,0 +1,9 @@
+﻿using AirlineCompany3.Model.Dto;
+
+namespace AirlineCompany3.Service.Interface
+{
+    public interface IFlightService
+    {
+        String create(FlightCreationDto flightDto);
+    }
+}
