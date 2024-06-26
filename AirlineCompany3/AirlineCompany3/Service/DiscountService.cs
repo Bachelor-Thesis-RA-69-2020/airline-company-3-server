fcaab27f361703930d3294f7e3df8bc14d53a03b
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:57:10 +0200

    feat: Added Discount service.

---
+++ AirlineCompany3/AirlineCompany3/Service/DiscountService.cs
@@ -0,0 +1,18 @@
+﻿using AirlineCompany3.Repository.Interface;
+using AirlineCompany3.Service.Interface;
+using AutoMapper;
+
+namespace AirlineCompany3.Service
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly IMapper _mapper;
+        private readonly IFlightRepository _flightRepository;
+
+        public DiscountService(IMapper mapper, IFlightRepository flightRepository)
+        {
+            _mapper = mapper;
+            _flightRepository = flightRepository;
+        }
+    }
+}
