8e249fec75329fcd485ffef1fd4796cefaf185b3
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:54:33 +0200

    feat: Added Discount creation DTO.

---
+++ AirlineCompany3/AirlineCompany3/Model/Dto/DiscountCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using AirlineCompany3.Model.Domain;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineCompany3.Model.Dto
+{
+    public class DiscountCreationDto
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public float DiscountPercentage { get; set; }
+        public string FlightSerialNumber { get; set; }
+    }
+}
