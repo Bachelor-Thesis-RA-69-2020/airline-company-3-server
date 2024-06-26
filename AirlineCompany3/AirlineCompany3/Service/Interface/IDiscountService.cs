00e0859b6ef9f8c0313f0d13dfed699448343c98
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 21:55:23 +0200

    feat: Added Discount service interface.

---
+++ AirlineCompany3/AirlineCompany3/Service/Interface/IDiscountService.cs
@@ -0,0 +1,9 @@
+﻿using AirlineCompany3.Model.Dto;
+
+namespace AirlineCompany3.Service.Interface
+{
+    public interface IDiscountService
+    {
+        MessageDto create(DiscountCreationDto discountDto);
+    }
+}
