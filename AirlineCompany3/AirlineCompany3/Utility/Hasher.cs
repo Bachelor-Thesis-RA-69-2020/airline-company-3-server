87ef829a801638208cc5f881c4b76e0a562bb109
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 19:11:30 +0200

    feat: Added Hasher utility.

---
+++ AirlineCompany3/AirlineCompany3/Utility/Hasher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AirlineCompany3.Utility
+{
+    public static class Hasher
+    {
+        public static string Hash(string entry)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(entry));
+                return BytesToHex(bytes);
+            }
+        }
+
+        private static string BytesToHex(byte[] hash)
+        {
+            StringBuilder hexString = new StringBuilder(2 * hash.Length);
+            foreach (byte b in hash)
+            {
+                string hex = b.ToString("x2");
+                hexString.Append(hex);
+            }
+            return hexString.ToString();
+        }
+    }
+}
