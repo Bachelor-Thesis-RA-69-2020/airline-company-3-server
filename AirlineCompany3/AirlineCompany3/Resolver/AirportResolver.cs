c7e181af87d759123481e66f52190d4c4ebd14af
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-26 18:48:45 +0200

    feat: Airports can now be fetched.

---
+++ AirlineCompany3/AirlineCompany3/Resolver/AirportResolver.cs
@@ -0,0 +1,25 @@
+﻿using AirlineCompany3.Model.Dto;
+using AirlineCompany3.Service.Interface;
+
+namespace AirlineCompany3.Resolver
+{
+    public class AirportResolver
+    {
+
+        public List<AirportDto> GetAirports(string? filter, [Service(ServiceKind.Synchronized)] IAirportService airportService)
+        {
+            List<AirportDto> airports = airportService.getAll();
+
+            if (string.IsNullOrEmpty(filter))
+            {
+                airports = airportService.getAll();
+            }
+            else
+            {
+                airports = airportService.search(filter);
+            }
+
+            return airports;
+        }
+    }
+}
