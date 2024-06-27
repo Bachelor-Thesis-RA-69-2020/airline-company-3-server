a0b2817ebd4bf941bc86139a6d76431edbb8747c
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-27 16:09:52 +0200

    feat: Added Booking Service.

---
+++ AirlineCompany3/AirlineCompany3/Service/BookingService.cs
@@ -0,0 +1,17 @@
+﻿using AirlineCompany3.Repository.Interface;
+using AutoMapper;
+
+namespace AirlineCompany3.Service
+{
+    public class BookingService
+    {
+        private readonly IMapper _mapper;
+        private readonly IFlightRepository _flightRepository;
+
+        public BookingService(IMapper mapper, IFlightRepository flightRepository)
+        {
+            _mapper = mapper;
+            _flightRepository = flightRepository;
+        }
+    }
+}
