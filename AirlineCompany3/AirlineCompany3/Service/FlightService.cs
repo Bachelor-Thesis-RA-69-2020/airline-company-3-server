b4af9568290d7aa90b91f59805568c8e7322d2c3
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-27 14:54:15 +0200

    feat: Flight search implementation.

--- AirlineCompany3/AirlineCompany3/Service/FlightService.cs
+++ AirlineCompany3/AirlineCompany3/Service/FlightService.cs
@@ -5,6 +5,8 @@ using AirlineCompany3.Repository.Interface;
 using AirlineCompany3.Service.Interface;
 using AirlineCompany3.Utility;
 using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
 
 namespace AirlineCompany3.Service
 {
@@ -50,6 +52,25 @@ namespace AirlineCompany3.Service
             }
         }
 
+        List<FlightDto> search(FlightSearchDto searchFilter)
+        {
+            List<Flight> flights = _flightRepository.GetAll(includedProperties: "Tickets,Discounts");
+
+            if (flights.IsNullOrEmpty())
+            {
+                return new List<FlightDto>();
+            }
+
+            flights = SearchBySerialNumber(flights, searchFilter.SerialNumber);
+            flights = SearchByDateRange(flights, searchFilter.From, searchFilter.To);
+            flights = SearchByRelation(flights, searchFilter.StartingPointIata, searchFilter.EndingPointIata);
+            flights = SearchByPassengerInformation(flights, searchFilter.FlightClass, searchFilter.PassengerCount);
+
+            List<FlightDto> flightDtos = _mapper.Map<List<FlightDto>>(flights);
+
+            return flightDtos;
+        }
+
         private String GenerateSerialNumber(Flight flight)
         {
             String id = flight.ScheduledDeparture.ToString() +
@@ -62,6 +83,129 @@ namespace AirlineCompany3.Service
             String serialNumber = "AL3-" + hash.Substring(0, 10).ToUpper();
 
             return serialNumber;
+        }
+
+        private List<Flight> SearchBySerialNumber(List<Flight> flights, string serialNumber)
+        {
+            if (serialNumber != null)
+            {
+                return flights;
+            }
+
+            flights = flights.Where(f => f.SerialNumber == serialNumber).ToList();
+
+            return flights;
+        }
+
+        private List<Flight> SearchByDateRange(List<Flight> flights, DateTime from, DateTime to)
+        {
+            if (from != null && to != null && from.CompareTo(to) > 0)
+            {
+                throw new ArgumentException("Validation: From date must be before To date.");
+            }
+
+            flights = SearchByFrom(flights, from);
+            flights = SearchByTo(flights, to);
+
+            return flights;
+        }
+
+        private List<Flight> SearchByRelation(List<Flight> flights, String startingPoint, String endingPoint)
+        {
+            if (startingPoint != null && endingPoint != null && startingPoint == endingPoint)
+            {
+                throw new ArgumentException("Validation: startingPoint and endingPoint cannot be the same.");
+            }
+
+            if (startingPoint != null)
+            {
+                _airportRepository.Get(a => a.Iata == startingPoint, orElseThrow: true);
+            }
+
+            if (endingPoint != null)
+            {
+                _airportRepository.Get(a => a.Iata == endingPoint, orElseThrow: true);
+            }
+
+            flights = SearchByStartingPoint(flights, startingPoint);
+            flights = SearchByArrivalAirport(flights, endingPoint);
+            return flights;
+        }
+
+        private List<Flight> SearchByPassengerInformation(List<Flight> flights, String flightClass, int passengerCount)
+        {
+            if (passengerCount == null)
+            {
+                passengerCount = 1;
+            }
+
+            if (passengerCount < 1)
+            {
+                throw new ArgumentException("Validation: Passenger count must be 1 or more.");
+            }
+
+            List<String> validFlightClasses = new List<string>();
+            validFlightClasses.AddRange(["Economy", "Business", "First"]);
+            if (flightClass != null && !validFlightClasses.Contains(flightClass))
+            {
+                throw new ArgumentException("Validation: Invalid flight class. Must be one of: Economy, Business, First.");
+            }
+
+            FlightClass flightClassFilter = Model.Domain.FlightClass.Economy;
+
+            if (flightClass != null && flightClass == "Economy")
+            {
+                flightClassFilter = Model.Domain.FlightClass.Economy;
+            }
+            else if (flightClass != null && flightClass == "Business")
+            {
+                flightClassFilter = Model.Domain.FlightClass.Business;
+            }
+            else if (flightClass != null && flightClass == "First")
+            {
+                flightClassFilter = Model.Domain.FlightClass.First;
+            }
+
+            flights = flights.Where(f => f.GetTicketCountByClass(flightClassFilter) >= passengerCount).ToList();
+            return flights;
+        }
+
+        private List<Flight> SearchByFrom(List<Flight> flights, DateTime from)
+        {
+            if (from != null)
+            {
+                flights = flights.Where(f => f.ScheduledDeparture.CompareTo(from) > 0).ToList();
+            }
+            return flights;
+        }
+
+        private List<Flight> SearchByTo(List<Flight> flights, DateTime to)
+        {
+            if (to != null) 
+            { 
+                flights = flights.Where(f => f.ScheduledDeparture.CompareTo(to) < 0).ToList();
+            }
+            return flights;
+        }
+
+        private List<Flight> SearchByStartingPoint(List<Flight> flights, String startingPoint)
+        {
+            if (startingPoint != null)
+            {
+
+                flights = flights.Where(f => f.StartingPoint.Iata == startingPoint).ToList();
+            }
+            return flights;
+        }
+
+        private List<Flight> SearchByArrivalAirport(List<Flight> flights, String endingPoint)
+        {
+            if (endingPoint != null)
+            {
+
+                flights = flights.Where(f => f.EndingPoint.Iata == endingPoint).ToList();
+            }
+            return flights;
         }
     }
 }
