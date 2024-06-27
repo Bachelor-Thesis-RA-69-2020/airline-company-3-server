5982ca29c11ec751283d4e8c91f2b36051c65ff3
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-27 15:54:35 +0200

    feat: Added Passenger DTO.

---
+++ AirlineCompany3/AirlineCompany3/Model/Dto/PassengerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AirlineCompany3.Model.Dto
+{
+    public class PassengerDto
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Passport { get; set; }
+    }
+}
