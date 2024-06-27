318dc880aca8d394cce302b8a396e4644f7f7a5a
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-27 15:53:26 +0200

    feat: Added Booking class.

---
+++ AirlineCompany3/AirlineCompany3/Model/Domain/Booking.cs
@@ -0,0 +1,35 @@
+﻿using AirlineCompany3.Models.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineCompany3.Model.Domain
+{
+    public class Booking : BaseEntity
+    {
+        [Required(ErrorMessage = "Validation: Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Validation: Email name is required")]
+        [EmailAddress(ErrorMessage = "Validation: Email should be valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Validation: Birth date is required")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Validation: Passport is required")]
+        public string Passport { get; set; }
+
+        public Booking() : base()
+        {
+        }
+
+        public override void Validate()
+        {
+            base.Validate();
+
+            if (BirthDate > DateTime.Now)
+            {
+                throw new ArgumentException("Validation: Birth date must be in the past.");
+            }
+        }
+    }
+}
