9687f76f278ad263e1fa7f0853e355d3193908bf
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-27 16:08:38 +0200

    feat: Added Booking service interface.

---
+++ AirlineCompany3/AirlineCompany3/Service/Interface/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using AirlineCompany3.Model.Domain;
+using AirlineCompany3.Model.Dto;
+
+namespace AirlineCompany3.Service.Interface
+{
+    public interface IBookingService
+    {
+        MessageDto Book(BookingDto bookingDto);
+    }
+}
