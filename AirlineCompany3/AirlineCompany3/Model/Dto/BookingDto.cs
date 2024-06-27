fb4f2808b3c388fbbe7b89f2b8422dd81d3bd17e
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-27 16:02:24 +0200

    feat: Configured Booking mapper.

---
+++ AirlineCompany3/AirlineCompany3/Model/Dto/BookingDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AirlineCompany3.Model.Dto
+{
+    public class BookingDto
+    {
+        public string FlightSerialNumber { get; set; }
+        public string FlightClass { get; set; }
+        public string Email { get; set; }
+
+        public List<PassengerDto> Passengers { get; set; }
+    }
+}
---
+++ AirlineCompany3/AirlineCompany3/Model/MappingProfile/BookingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AirlineCompany3.Model.Domain;
+using AirlineCompany3.Model.Dto;
+using AutoMapper;
+
+namespace AirlineCompany3.Model.MappingProfile
+{
+    public class BookingProfile : Profile
+    {
+        public BookingProfile()
+        {
+            CreateMaps();
+        }
+
+        private void CreateMaps()
+        {
+            CreateMap<BookingDto, List<Booking>>()
+                .ConvertUsing((src, dest, context) =>
+                {
+                    var bookings = new List<Booking>();
+                    foreach (var passenger in src.Passengers)
+                    {
+                        var booking = new Booking();
+                        booking.Name = passenger.Name;
+                        booking.BirthDate = passenger.BirthDate;
+                        booking.Passport = passenger.Passport;
+                        booking.Email = src.Email;
+
+                        bookings.Add(booking);
+                    }
+                    return bookings;
+                });
+        }
+    }
+}
