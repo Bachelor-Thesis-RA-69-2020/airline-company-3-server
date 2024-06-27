694b79f7c0c3a667a932f07d0f2186643f3c829a
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-27 14:26:48 +0200

    feat: Added Fliht search Dto.

---
+++ AirlineCompany3/AirlineCompany3/Model/Dto/FlightSearchDto.cs
@@ -0,0 +1,15 @@
+﻿using AirlineCompany3.Model.Domain;
+
+namespace AirlineCompany3.Model.Dto
+{
+    public class FlightSearchDto
+    {
+        public string SerialNumber { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public string StartingPointIata { get; set; }
+        public string EndingPointIata { get; set; }
+        public string FlightClass { get; set; }
+        public int PassengerCount { get; set; }
+    }
+}
